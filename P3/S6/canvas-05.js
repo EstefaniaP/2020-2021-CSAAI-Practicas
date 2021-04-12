7556d604b31d09928cd7a214b62b41615ee04f24
Estefania Portillo <estefaniaportillocastro@gmail.com>
2021-04-12 23:52:49 +0200

    semicirculo

--- P3/S6/canvas-05.js
+++ P3/S6/canvas-05.js
@@ -13,7 +13,7 @@ const ctx = canvas.getContext("2d");
 ctx.beginPath();
     //-- Dibujar un circulo: coordenadas x,y del centro
     //-- Radio, Angulo inicial y angulo final
-    ctx.arc(100, 50, 10, 0, 2 * Math.PI);
+    ctx.arc(100, 50, 10, 0, Math.PI);
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 3;
     ctx.fillStyle = 'yellow';
